0c92fe857d1d59bd5c8e245bf70614fe0fc5f282
u005 <47481735+u005@users.noreply.github.com>
2019-02-18 10:58:20 -0700

    Working Version 1

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+const mysql = require('mysql')
+const bodyParser = require('body-parser')
+const path = require('path')
+const ejs = require('ejs')
+
+app.use(bodyParser.urlencoded( {extended: false}))
+app.set('view engine', 'ejs')
+
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'localhost',
+    user: 'siyuan',
+    password: 'siyuan05',
+    database: 'nodejs'
+})
+
+function getConnection(){
+    return pool
+}
+
+app.get('/form', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/form.html'))
+    console.log("Connected to Form")
+})
+
+app.post('/test', (req, res) => {
+    const con = getConnection()
+    const firstName = req.body.first_name
+    const sql = "SELECT * FROM users WHERE first_name = ?"
+    const insert = "INSERT INTO users (first_name) values (?)"
+    con.query(sql, [firstName], (err, rows) => {
+        if (err) {
+            sendstatus(500)
+            console.log("Failed to find user")
+            return
+        }
+    rows.map((row) => {
+        const user = {user: {firstName: row.first_name}}
+        res.render('test.ejs', user)
+        })
+    })
+})
+
+app.listen(3012, () => console.log("App Running on 3012"))
