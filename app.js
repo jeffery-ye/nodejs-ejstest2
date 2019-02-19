634f052e5821544528b424385083b44c0efa5955
u005 <47481735+u005@users.noreply.github.com>
2019-02-18 19:20:09 -0700

    Insert User Function Added

--- app.js
+++ app.js
@@ -28,8 +28,7 @@ app.get('/form', (req, res) => {
 app.post('/test', (req, res) => {
     const con = getConnection()
     const firstName = req.body.first_name
-    const sql = "SELECT * FROM users WHERE first_name = ?"
-    const insert = "INSERT INTO users (first_name) values (?)"
+    const sql = "SELECT * FROM users WHERE first_name = ? limit 1"
     con.query(sql, [firstName], (err, rows) => {
         if (err) {
             sendstatus(500)
@@ -40,7 +39,28 @@ app.post('/test', (req, res) => {
         const user = {user: {firstName: row.first_name}}
         res.render('test.ejs', user)
         })
+        
     })
 })
+
+
+app.get('/create', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/outputform.html'))
+    console.log("Connected to Output Form")
+})
+
+app.post('/output', (req, res) => {
+    const con = getConnection()
+    const firstName = req.body.first_name
+    const sql = "INSERT INTO users (first_name) values (?)"
+    con.query(sql, [firstName], (err, rows) => {
+        const user = {user: {"firstName": firstName}}
+        //const user = {user: {firstName: row.first_name}}
+        res.render('output.ejs', user)
+    })        
+})
+
+
+
 
 app.listen(3012, () => console.log("App Running on 3012"))
